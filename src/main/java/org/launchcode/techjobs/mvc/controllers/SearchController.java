6de7c3b0c649d2dfdb3a178e2a103978827106b1
Sebastian Brown <108308812+Sebastian-Brown@users.noreply.github.com>
2022-10-27 15:46:39 -0500

    Final commit

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -1,9 +1,16 @@
 package org.launchcode.techjobs.mvc.controllers;
 
+import org.launchcode.techjobs.mvc.models.Job;
+import org.launchcode.techjobs.mvc.models.JobData;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.Locale;
 
 import static org.launchcode.techjobs.mvc.controllers.ListController.columnChoices;
 
@@ -21,6 +28,17 @@ public class SearchController {
         return "search";
     }
 
-    // TODO #3 - Create a handler to process a search request and render the updated search view.
+    @PostMapping(value = "results")
+    public String displaySearchResults(Model model, @RequestParam String  searchType, @RequestParam String searchTerm) {
+      ArrayList<Job> searchJobs;
 
+        if (searchType.toLowerCase(Locale.ROOT) == "all" || searchTerm == "") {
+            searchJobs = JobData.findAll();
+        } else {
+            searchJobs = JobData.findByColumnAndValue(searchType, searchTerm);
+        }
+        model.addAttribute("jobs", searchJobs);
+        model.addAttribute("columns", columnChoices);
+        return "search";
+    }
 }
